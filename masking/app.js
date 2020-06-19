d2d8639b8cac6876edd073dd46566ed9a07ffe15
ty18881 <ty18881@gmail.com>
2020-06-18 20:08:52 -0400

    Robin.io - Regex - masking digits in a number

---
+++ masking/app.js
@@ -0,0 +1,19 @@
+// mask all but the last four numbers of the input provided
+// cannot assume all numbers are multiples of four
+// FREX AMEX numbers are 15 digits
+
+const maskDigits = (cardNum) => {
+
+    let cardString = cardNum.toString();
+
+// learned the Look Ahead assertion today.
+// below matches digits only if they are immediately followed by 4 digits.
+// the 4 digits are not returned in the match results.
+    return cardString.replace(/\d(?=\d{4})/g, '*');
+}
+
+console.log(maskDigits(88888888));
+
+console.log(maskDigits(121212121212));
+
+console.log(maskDigits(151515151515151));
